e46c1974a6fe7d6e656f8a2b87dd71010f36f984
Tobias Rahloff <rahloff@gmail.com>
2017-04-03 23:19:09 +0200

    fixed test cases

--- components/routes/quoteRoutes.js
+++ components/routes/quoteRoutes.js
@@ -3,7 +3,9 @@ const api = require('express').Router()
 const quotes = require(path.join(__dirname, '/../pratchett.json'))
 
 api.get('/getRandom', (req, res) => {
-  res.send(quotes[Math.floor(Math.random() * quotes.length)])
+  res.send({
+    quote: quotes[Math.floor(Math.random() * quotes.length)]
+  })
 })
 
 module.exports = api
